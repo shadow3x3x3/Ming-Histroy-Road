3494545a675fefa3fda3144bd40577b6aa862661
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-07-20 16:59:30 +0800

    add find edges neighbors

--- core/edge_generator.rb
+++ core/edge_generator.rb
@@ -1,11 +1,35 @@
 require_relative 'edge'
+require_relative 'util/graph_util'
 
 class EdgeGenerator
+  include GraphUtil
+
+  attr_reader :edges_neighbors
+
   def initialize(edges)
     edges.each do |e|
       unless e.class == Edge
         raise ArgumentError, 'every elements in edges need to be a Edge class'
       end
     end
+    @edges = edges
   end
+
+  def find_edges_combination(edges)
+    sum_edge = 0
+    @edges_neighbors = {}
+    edges.each do |edge|
+      @edges_neighbors[edge.id.to_s] = find_edge_neighbors(edge)
+    end
+  end
+
+  def find_edge_neighbors(edge)
+    included_edges = find_edge_include(edge.dst)
+    included_edges += find_edge_include(edge.src)
+    included_edges.delete(edge)
+    included_edges.uniq!
+  end
+
+
+
 end
---
+++ core/util/graph_util.rb
@@ -0,0 +1,44 @@
+module GraphUtil
+  def find_neighbors(node)
+    neighbors = []
+    @edges.each do |edge|
+      neighbors << check_neighbor(node, edge)
+    end
+    neighbors.compact!
+  end
+
+  def check_neighbor(node, edge)
+    case node
+    when edge.src
+      return edge.dst
+    when edge.dst
+      return edge.src
+    end
+  end
+
+  def find_edge(src, dst)
+    @edges.each do |edge|
+      return edge if edge.src == src && edge.dst == dst
+      return edge if edge.src == dst && edge.dst == src
+    end
+    raise ArgumentError, "not connect between #{src} and #{dst}"
+  end
+
+  def find_edge_include(node)
+    result_edges = []
+    @edges.each do |edge|
+      result_edges << edge if edge.src == node || edge.dst == node
+    end
+    result_edges
+  end
+
+  def same_edge?(edge1, edge2)
+    edge1_src_id = edge1.src
+    edge1_dst_id = edge1.dst
+    edge2_src_id = edge2.src
+    edge2_dst_id = edge2.dst
+    return true if edge1_src_id == edge2_src_id && edge1_dst_id == edge2_dst_id
+    return true if edge1_dst_id == edge2_src_id && edge1_src_id == edge2_dst_id
+    false
+  end
+end
