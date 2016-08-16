50c147d7ab5fb1a0420a1b8206ac6611a47512bb
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-08-16 22:59:48 +0800

    fix output path

--- preprocessing/IO/output_to_txt.rb
+++ preprocessing/IO/output_to_txt.rb
@@ -1,14 +1,14 @@
 module OutputToTxt
   def write_edges
-    File.open('./output/ming_edges.txt', "w") do |f|
+    File.open('../output/ming2_edges.txt', "w") do |f|
       @edges.each do |edge|
-        f.write("#{edge.id}   #{edge.src}   #{edge.dst}   #{edge.dist}   #{random}   #{random}\n")
+        f.write("#{edge.id}   #{edge.src}   #{edge.dst}   #{edge.dist}\n")
       end
     end
   end
 
   def write_nodes
-    File.open('./output/ming_nodes.txt', "w") do |f|
+    File.open('../output/ming_nodes.txt', "w") do |f|
       @nodes.each do |node|
         f.write("#{node.id}   #{node.name}   #{node.long}   #{node.lat}\n")
       end
