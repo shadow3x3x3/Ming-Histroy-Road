314709a99298b27464d04720b18f2d59528415ee
shadow3x3x3 <shadow3x3x3@gmail.com>
2016-09-02 22:04:07 +0800

    remove head for unknow edge nums

--- training-data-process/structure/IO/output_util.rb
+++ training-data-process/structure/IO/output_util.rb
@@ -2,17 +2,13 @@ require 'csv'
 
 module OutputUtil
   def self.output_start_codes_csv(path, dataset)
-    CSV.open(path, "wb",
-      write_headers: true,
-      headers: ["a", "b", "c", "d", "days"]) do |csv|
+    CSV.open(path, "wb") do |csv|
       dataset.each { |data| csv << data }
     end
   end
 
   def self.output_setting_csv(path, dataset)
-    CSV.open(path, "wb",
-      write_headers: true,
-      headers: ["a", "b", "c", "d"]) do |csv|
+    CSV.open(path, "wb") do |csv|
       csv << dataset
     end
   end
